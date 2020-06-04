555a85cd0ead66e537ea2cbd78b8ad8db7ac513e
deping <cdp97531@sina.com>
2020-06-04 09:00:08 +0800

    refined code style to fix eslint error and format error.

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,14 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+    "space-before-function-paren": ["error", "never"],
+    camelcase: "off",
+    '@typescript-eslint/camelcase': 'off',
+    "indent": ["error", 2, {
+      "SwitchCase": 1
+    }],
   }
 }
