8373f1a62ad10ea8cc1d30c94ece509edcb3f653
deping <cdp97531@sina.com>
2020-06-11 12:56:08 +0800

    add models of CaoxingZuheliang

---
+++ src/components/CaoXingZuHeLiang/models/ui-data.ts
@@ -0,0 +1,46 @@
+export class UiData {
+  isEmpty!: string;
+  valueL!: string;
+  zdhlform!: string;
+  dfbData!: string;
+  dfbOption!: string;
+  sllxData!: {
+    [prop: string]: string;
+  };
+  sllxOption!: {
+    [prop: string]: string;
+  };
+  hglxData!: {
+    [prop: string]: string;
+  };
+  hglxOption!: {
+    [prop: string]: string;
+  };
+  rklxData!: {
+    [prop: string]: string;
+  };
+  rklxOption!: {
+    [prop: string]: string;
+  };
+  hllxData!: {
+    [prop: string]: string;
+  };
+  hllxOption!: {
+    [prop: string]: string;
+  };
+  zzgsData!: {
+    [prop: string]: string;
+  };
+  zcjjllxData!: {
+    [prop: string]: string;
+  };
+  zcjjllxOption!: {
+    [prop: string]: string;
+  };
+  zdhllxData!: {
+    [prop: string]: string;
+  };
+  zdhllxOption!: {
+    [prop: string]: string;
+  };
+}
