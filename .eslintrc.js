a74bb240e8c79abd918747a224894614645d4ec6
deping <cdp97531@sina.com>
2020-06-12 17:46:01 +0800

    quote-props

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     "semi": ["error", "always"],
     "quotes": ["error", "double"],
+    "quote-props": ["error", "as-needed", { "numbers": true }],
     "space-before-function-paren": ["error", "never"],
     camelcase: "off",
     '@typescript-eslint/camelcase': 'off',
